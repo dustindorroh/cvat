06afc2cd859a6c9ac6e96c1cc174d77750347020
Dustin Dorroh <dustindorroh@gmail.com>
2019-07-25 03:32:57 -0700

    WIP Use custom graph for tf_annotation process

--- components/tf_annotation/install.sh
+++ components/tf_annotation/install.sh
@@ -6,11 +6,14 @@
 #
 set -e
 
+#cd ${HOME} && \
+#wget -O model.tar.gz http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz && \
+#tar -xzf model.tar.gz && rm model.tar.gz && \
+#mv faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28 ${HOME}/rcnn && cd ${HOME} && \
+#mv rcnn/frozen_inference_graph.pb rcnn/inference_graph.pb
 cd ${HOME} && \
-wget -O model.tar.gz http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz && \
-tar -xzf model.tar.gz && rm model.tar.gz && \
-mv faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28 ${HOME}/rcnn && cd ${HOME} && \
-mv rcnn/frozen_inference_graph.pb rcnn/inference_graph.pb
+mkdir ${HOME}/rcnn && \
+mv /tmp/components/tf_annotation/frozen_inference_graph.pb rcnn/inference_graph.pb
 
 if [[ "$CUDA_SUPPORT" = "yes" ]]
 then
